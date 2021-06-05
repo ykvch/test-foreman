37ad060c77baa56b084fa267ccbf5c02bfa491d2
ykvch <yankevych@gmail.com>
2021-06-05 23:45:05 +0300

    Client docstrings.

--- testforeman/client.py
+++ testforeman/client.py
@@ -9,6 +9,16 @@ class Client:
         self.sock, self.addr = None, addr
 
     def say(self, message):
+        """
+        Base method to communicate with foreman-server.
+        Auto-connects if needed.
+
+        Args:
+            message (str): message to send
+
+        Returns:
+            str: response from server
+        """
         if not self.sock:
             self.sock = socket.socket()
             self.sock.settimeout(8)
@@ -18,7 +28,7 @@ class Client:
 
         # Double LF denotes end of response
         buff = b""
-        while b"\n\n" not in buff:
+        while b"\n\n" not in buff:  # IDEA: avoid scan entire buff on each recv?
             resp = self.sock.recv(1024)
             if not resp:
                 self.close()
@@ -33,9 +43,23 @@ class Client:
             self.sock = None
 
     def take(self, name):
+        """
+        Ask foreman-server permission to take `name`.
+        
+        Args:
+            name (str): resource name to claim
+        
+        Returns:
+            bool: True - take is successful. False - name already taken.
+
+        If foreman responds `name 0` -- noone's been asking it yet. We take it.
+        Otherwise foreman says `name 1` -- it's been claimed before. Take fails.
+        """
         resp = self.say(f"take {name}")
+        # Response comes in format: `{name} <zero or one>`
         resp_name, taken = resp[:-2], resp[-1]
         assert name == resp_name, "Take name mismatch"
+        # Zero means: noone else claimed {name} yet, take is successful.
         return int(taken) == 0
 
     def stop_foreman(self):
